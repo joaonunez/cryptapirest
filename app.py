a71f058fd50ce98696e292f710318d19eb7d4df7
joaonunez <joaovaldiglesias@gmail.com>
2024-09-24 22:41:59 -0300

    app added

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello")
+import os
+import sys
+
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def app:app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It works!\n'
+    version = 'Python v' + sys.version.split()[0] + '\n'
+    response = '\n'.join([message, version])
+    return [response.encode()]
